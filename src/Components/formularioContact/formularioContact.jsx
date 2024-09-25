889877eb317d6518dbc053f89ef0fcbdf8174346
Eder <julioo1122@gmail.com>
2024-09-25 03:22:41 -0600

    update

--- src/Components/formularioContact/formularioContact.jsx
+++ src/Components/formularioContact/formularioContact.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import "./formularioContact.css"; // Importamos los estilos del formulario
-import imgGps from '../../assets/images/gps.png'
-import imgMail from '../../assets/images/mail.png'
-import imgPhone from '../../assets/images/phone.png'
-import facebookIcon from '../../assets/images/facebookIcon.png'
-import xIcon from '../../assets/images/xIcon.png'
+import imgGps from '../../assets/images/gps.png';
+import imgMail from '../../assets/images/mail.png';
+import imgPhone from '../../assets/images/phone.png';
+import facebookIcon from '../../assets/images/facebookIcon.png';
+import xIcon from '../../assets/images/xIcon.png';
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
   const [email, setEmail] = useState("");
@@ -13,8 +14,21 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Lógica para enviar los datos del formulario
-    console.log({ email, message });
+
+    const templateParams = {
+      from_email: email,
+      message: message,
+    };
+
+    emailjs.send('service_vtg818f', 'template_nlaarop', templateParams, 'Q50hqWUqP14JWpVbC')
+      .then((response) => {
+        console.log('Email sent successfully!', response.status, response.text);
+        // Limpiar el formulario
+        setEmail('');
+        setMessage('');
+      }, (error) => {
+        console.error('Failed to send email:', error);
+      });
   };
 
   return (
@@ -57,7 +71,6 @@ const ContactForm = () => {
           <h1 className="right-title">Contáctanos</h1>
           <div className="title-underline"></div>
           <div className="contact-items">
-            
             <div className="contact-item">
               <div className="contact-item-header">
                 <img src={imgGps} alt="icon" className="icon" />
@@ -66,7 +79,6 @@ const ContactForm = () => {
               <p className="contact-item-text">Privada, Torres de La Fuente, Plaza las Torres, 42082 Pachuca de Soto, Hidalgo, México.</p>
             </div>
 
-              
             <div className="contact-item">
               <div className="contact-item-header">
                 <img src={imgPhone} alt="icon" className="icon" />
@@ -74,16 +86,14 @@ const ContactForm = () => {
               </div>
               <p className="contact-item-text">771 148 6170</p>
             </div>
-              
+            
             <div className="contact-item">
               <div className="contact-item-header">
                 <img src={imgMail} alt="icon" className="icon" />
-                <h3 className="contact-item-title">Correo eléctronico</h3>
+                <h3 className="contact-item-title">Correo electrónico</h3>
               </div>
               <p className="contact-item-text">fundacionaxiomexico@gmail.com</p>
             </div>
-
-       
           </div>
         </div>
       </div>
@@ -92,41 +102,32 @@ const ContactForm = () => {
       <div className="find-us-section">
         <h2 className="find-us-title">Encuéntranos</h2>
         <div className="social-icons">
-          <img
-            src={facebookIcon}
-            alt="Facebook"
-            className="social-icon"
-          />
+          <img src={facebookIcon} alt="Facebook" className="social-icon" />
           <img src={xIcon} alt="X" className="social-icon" />
         </div>
       </div>
 
       <div className="contact-section">
-      <h1 className="right-title">Soporte Técnico</h1>
-      <div className="title-underline"></div>
-      <div className="contact-items">
-                
-      <div className="contact-item">
-              <div className="contact-item-header">
-                <img src={imgMail} alt="icon" className="icon" />
-                <h3 className="contact-item-title">Correo eléctronico</h3>
-              </div>
-              <p className="contact-item-text">axio.valores@gmail.com</p>
+        <h1 className="right-title">Soporte Técnico</h1>
+        <div className="title-underline"></div>
+        <div className="contact-items">
+          <div className="contact-item">
+            <div className="contact-item-header">
+              <img src={imgMail} alt="icon" className="icon" />
+              <h3 className="contact-item-title">Correo electrónico</h3>
             </div>
+            <p className="contact-item-text">axio.valores@gmail.com</p>
+          </div>
+
+          <div className="contact-item">
+            <div className="contact-item-header">
+              <img src={imgPhone} alt="icon" className="icon" />
+              <h3 className="contact-item-title">Teléfono</h3>
+            </div>
+            <p className="contact-item-text">771 148 6170</p>
+          </div>
         </div>
-
-                     
-          
-        <div className="contact-item">
-              <div className="contact-item-header">
-                <img src={imgPhone} alt="icon" className="icon" />
-                <h3 className="contact-item-title">Teléfono</h3>
-              </div>
-              <p className="contact-item-text">771 148 6170</p>
-            </div>
-
       </div>
-
     </div>
   );
 };
