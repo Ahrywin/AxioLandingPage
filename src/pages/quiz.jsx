3363e5922376e42aa6bad3a980c740ba1abb3009
Cocoyanami <cocoyanomami@gmail.com>
2024-11-07 12:15:32 -0600

    quiz

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,206 +1,194 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; // Importa la función para generar UUIDs
 import MultipleChoiceQuestion from '../Components/MultipleChoiceQuestion/MultipleChoiceQuestion';
 import './quiz.css';
 import ImgDigital1 from '../assets/images/eco.jpg';
 
-import BannerComp from '../Components/BannerComp/BannerComp'
-import Footer from '../Footer'
+import BannerComp from '../Components/BannerComp/BannerComp';
+import Footer from '../Footer';
+
 function Quiz() {
   const questions = [
     {
-      //consejo
       question: 'Es una obligación moral consultar a los expertos antes de decidir.',
-      value: 1, // Value for this question
+      value: 1,
       options: [
         { value: 0, label: 'Indiferente' },
         { value: -2, label: 'Totalmente en desacuerdo' },
         { value: -1, label: 'En desacuerdo' },
         { value: 1, label: 'De acuerdo' },
-        { value: 2, label: 'Totalmente de acuerdo' }
-      ]
+        { value: 2, label: 'Totalmente de acuerdo' },
+      ],
     },
     {
       question: 'Antes de tomar una decisión siempre estoy dispuesto a recibir un buen consejo.',
-      value: 2, // Value for this question
+      value: 2,
       options: [
         { value: 0, label: 'Indiferente' },
         { value: -2, label: 'Totalmente en desacuerdo' },
         { value: -1, label: 'En desacuerdo' },
         { value: 1, label: 'De acuerdo' },
-        { value: 2, label: 'Totalmente de acuerdo' }
-      ]
-    }
+        { value: 2, label: 'Totalmente de acuerdo' },
+      ],
+    },
   ];
 
+  const organizations = [
+    { id: 'db4875a9-1587-46c3-a068-12ba39d19250', name: 'ORGANIZACOIN AXIO' }
+  ];
 
-  const [email, setEmail] = useState('');
-  const [emailSubmitted, setEmailSubmitted] = useState(false); // Para controlar si se ha ingresado el email
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const departments = [
+    { id: '8760b3a5-d269-462d-8aa9-e6a6931e58ab', name: 'CA-TEE SISTEMAS' }
+  ];
+
+  const [organizationId, setOrganizationId] = useState('');
+  const [departmentId, setDepartmentId] = useState('');
+  const [currentStep, setCurrentStep] = useState(0); // Controla todos los pasos: organización, departamento, preguntas
   const [answers, setAnswers] = useState(Array(questions.length).fill(null));
 
   const handleOptionSelect = (selectedValue) => {
     const newAnswers = [...answers];
-    newAnswers[currentQuestionIndex] = selectedValue;
+    newAnswers[currentStep - 2] = selectedValue; // Ajustar el índice para almacenar la respuesta correcta
     setAnswers(newAnswers);
   };
 
   const handleNext = () => {
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    if (currentStep < questions.length + 1) { // Asegúrate de que solo avance hasta las preguntas
+      setCurrentStep(currentStep + 1);
     }
   };
 
   const handlePrevious = () => {
-    if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
     }
   };
 
   const handleSubmit = async () => {
-    const sectionResults = {}; // Guardará los resultados por categoría
+    const quizId = uuidv4(); // Genera un nuevo ID único para cada quiz
 
     const results = questions.map((question, index) => {
-        const category = getCategory(question.value);
-        
-        // Inicializa la categoría si no existe en sectionResults
-        if (!sectionResults[category]) {
-            sectionResults[category] = 0;
-        }
-
-        // Suma la respuesta seleccionada a la categoría correspondiente
-        if (answers[index] !== null) {
-            sectionResults[category] += answers[index];
-        }
-
-        return {
-            question: question.value,  // Envía el valor de la pregunta
-            answer: answers[index],     // Envía el valor de la respuesta seleccionada
-            category: category, // Categoría obtenida
-        };
+      return {
+        quizId,  // El quizId ahora es dinámico
+        question: question.value,  // Enviar solo el valor de la pregunta
+        answer1: answers[index],     // Enviar la respuesta seleccionada
+      };
     });
 
     const body = {
-        email: email, // Asegúrate de tener el valor del correo electrónico
-        answers: results,
-        sectionResults: sectionResults // Envía también los resultados por sección
+      id: quizId,  // El id es el mismo que el quizId
+      organizationId,
+      departmentId, // Enviar solo el ID del departamento
+      createdAt: new Date().toISOString(),
+      answers: results,
     };
 
-    // Enviar datos al backend
     try {
-        const response = await fetch('https://axiobk-001-site2.ktempurl.com/quiz', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body), // Usa el objeto body aquí
-        });
+      const response = await fetch('https://axiobk-001-site2.ktempurl.com/api/quiz', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
 
-        if (response.ok) {
-            alert('Respuestas guardadas con éxito.');
-        } else {
-            alert('Error al guardar las respuestas.');
-        }
-    } catch (error) {
-        console.error('Error:', error);
+      if (response.ok) {
+        alert('Respuestas guardadas con éxito.');
+      } else {
         alert('Error al guardar las respuestas.');
-    }
-};
-
-const categories = [
-  { limit: 4, name: "Consejo" },
-  { limit: 8, name: "Juicio" },
-  { limit: 12, name: "Imperio" },
-  { limit: 16, name: "Memoria del pasado" },
-  { limit: 20, name: "Circunspección" },
-  { limit: 24, name: "Cautela" },
-  { limit: 28, name: "Perseverancia" },
-  { limit: 32, name: "Constancia" },
-  { limit: 36, name: "Paciencia" },
-  { limit: 40, name: "Magnanimidad" },
-  { limit: 44, name: "Cultura de la legalidad" },
-  { limit: 48, name: "Justicia distributiva" },
-  { limit: 52, name: "Justicia conmutativa" },
-  { limit: 56, name: "Gratitud" },
-  { limit: 60, name: "Fidelidad" },
-  { limit: 64, name: "Respeto" },
-  { limit: 68, name: "Liberalidad" },
-  { limit: 72, name: "Honestidad" },
-  { limit: 76, name: "Moderación" },
-  { limit: 80, name: "Sobriedad" },
-  { limit: 84, name: "Humildad" },
-  { limit: 88, name: "Simplicidad" },
-  { limit: 90, name: "Estudiosidad" },
-];
-
-const getCategory = (questionValue) => {
-  for (let i = 0; i < categories.length; i++) {
-      if (questionValue <= categories[i].limit) {
-          return categories[i].name;
       }
-  }
-  return "desconocido"; // Valor por defecto si el rango es mayor que 90
-};
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error al guardar las respuestas.');
+    }
+  };
 
-  
-
-  // Progreso de la barra basado en el email y las preguntas
-  const totalSteps = questions.length + 1; // Una para el email
-  const progressPercentage = ((currentQuestionIndex + 1 + (emailSubmitted ? 1 : 0)) / totalSteps) * 100;
+  const totalSteps = questions.length + 2; // Incluyendo los pasos de organización y departamento
+  const progressPercentage = ((currentStep + 1) / totalSteps) * 100;
 
   return (
     <div>
-    <BannerComp title={"Quiz"} image={ImgDigital1}/>
-    <div className="App">
-        
-      <div className="progress-bar" style={{ width: `${progressPercentage}%` }}></div>
+      <BannerComp title={'Quiz'} image={ImgDigital1} />
+      <div className="App">
+        <div className="progress-bar" style={{ width: `${progressPercentage}%` }}></div>
 
-      {/* Mostrar el campo de email solo si no se ha ingresado */}
-      {!emailSubmitted && (
-        <div className="question-container">
-          <h3>Ingresa tu correo para avanzar</h3>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            required
-            className='ema'
+        {/* Paso de selección de organización */}
+        {currentStep === 0 && (
+          <div className="question-container">
+            <h3>Selecciona una organización para avanzar</h3>
+            <select
+              value={organizationId}
+              onChange={(e) => setOrganizationId(e.target.value)}
+              required
+              className="organization-select"
+            >
+              <option value="" disabled>
+                Selecciona una organización
+              </option>
+              {organizations.map((org) => (
+                <option key={org.id} value={org.id}>
+                  {org.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {/* Paso de selección de departamento */}
+        {currentStep === 1 && organizationId && (
+          <div className="question-container">
+            <h3>Selecciona un departamento para avanzar</h3>
+            <select
+              value={departmentId}
+              onChange={(e) => setDepartmentId(e.target.value)}
+              required
+             className="organization-select"
+            >
+              <option value="" disabled>
+                Selecciona un departamento
+              </option>
+              {departments.map((dept) => (
+                <option key={dept.id} value={dept.id}>
+                  {dept.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+
+        {/* Mostrar preguntas solo si se ha seleccionado la organización y el departamento */}
+        {currentStep >= 2 && organizationId && departmentId && (
+          <MultipleChoiceQuestion
+            question={questions[currentStep - 2].question}
+            options={questions[currentStep - 2].options}
+            onOptionSelect={handleOptionSelect}
+            selectedOption={answers[currentStep - 2]}
+            questionIndex={currentStep - 2}
           />
-          <br/><br/><br/>
-          <button onClick={() => setEmailSubmitted(true)} disabled={!email} className='qzbutton'>
-            Seguir
-          </button>
-        </div>
-      )}
+        )}
 
-      {/* Mostrar preguntas solo si el email ha sido ingresado */}
-      {emailSubmitted && (
-        <MultipleChoiceQuestion
-          question={questions[currentQuestionIndex].question}
-          options={questions[currentQuestionIndex].options}
-          onOptionSelect={handleOptionSelect}
-          selectedOption={answers[currentQuestionIndex]}
-          questionIndex={currentQuestionIndex}
-        />
-      )}
-
-      {emailSubmitted && (
+        {/* Botones de navegación */}
         <div className="button-container">
-          <button onClick={handlePrevious} disabled={currentQuestionIndex === 0} className='qzbutton'>
+          <button onClick={handlePrevious} disabled={currentStep === 0} className='qzbutton'>
             Anterior
           </button>
-          <button onClick={handleNext} disabled={answers[currentQuestionIndex] === null} className='qzbutton'>
-            Siguiente
-          </button>
-          {currentQuestionIndex === questions.length - 1 && (
-            <button onClick={handleSubmit} disabled={!email} className='qzbutton'>
+
+          {/* Mostrar "Siguiente" solo si no estamos en el último paso */}
+          {currentStep < questions.length + 1 && (
+            <button onClick={handleNext} disabled={answers[currentStep - 2] === null} className='qzbutton'>
+              Siguiente
+            </button>
+          )}
+
+          {/* Mostrar "Finalizar" en la última pregunta */}
+          {currentStep === questions.length + 1 && (
+            <button onClick={handleSubmit} disabled={answers[currentStep - 2] === null} className='qzbutton'>
               Finalizar
             </button>
           )}
         </div>
-      )}
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </div>
   );
 }
