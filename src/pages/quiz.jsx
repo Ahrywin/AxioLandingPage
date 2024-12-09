2524c9488971b159e6db43a23e5a59238db0edeb
Cocoyanami <cocoyanomami@gmail.com>
2024-12-09 11:15:03 -0600

    check bug

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useQuizService } from '../Components/MultipleChoiceQuestion/quizService';
-import MultipleChoiceQuestion from '../Components/MultipleChoiceQuestion/MultipleChoiceQuestion';
-import BannerComp from '../Components/BannerComp/BannerComp';
-import QuizProgress from '../Components/MultipleChoiceQuestion/QuizProgres';
-import Finish from '../pages/finish';
-import './quiz.css';
-import { Helmet } from 'react-helmet';
-import ImgDigital1 from '../assets/images/eco.jpg';
-import Maintenance from "../Components/Mantenimiento/mantenimiento"
-import Wait from "../Components/Mantenimiento/Loading"
+import React, { useEffect, useState } from "react";
+import { useQuizService } from "../Components/MultipleChoiceQuestion/quizService";
+import MultipleChoiceQuestion from "../Components/MultipleChoiceQuestion/MultipleChoiceQuestion";
+import BannerComp from "../Components/BannerComp/BannerComp";
+import QuizProgress from "../Components/MultipleChoiceQuestion/QuizProgres";
+import Finish from "../pages/finish";
+import "./quiz.css";
+import { Helmet } from "react-helmet";
+import ImgDigital1 from "../assets/images/eco.jpg";
+import Maintenance from "../Components/Mantenimiento/mantenimiento";
+import Wait from "../Components/Mantenimiento/Loading";
 
 function Quiz() {
   const [quizData, setQuizData] = useState([]);
   const [departments, setDepartments] = useState([]);
   const [showFinish, setShowFinish] = useState(false);
-
-  // Estado temporal para guardar el departamento seleccionado
-  const [selectedDepartment, setSelectedDepartment] = useState(null);
-
   const [isFetching, setIsFetching] = useState(true); // Estado de carga
   const [isError, setIsError] = useState(false); // Estado de error
 
   const {
-    organizationId, setOrganizationId,
-    departamentId, setDepartmentId,
-    gener, setGener,
-    age, setAge,
-    educationLevel, setEducationLevel,
+    organizationId,
+    setOrganizationId,
+    departamentId,
+    setDepartmentId,
+    gener,
+    setGener,
+    age,
+    setAge,
+    educationLevel,
+    setEducationLevel,
     currentStep,
     answers,
     handleOptionSelect,
-    handleNext, handlePrevious,
+    handleNext,
+    handlePrevious,
     handleSubmit,
     randomQuestions,
     elapsedTime,
@@ -50,14 +52,16 @@ function Quiz() {
         setIsFetching(true);
         setIsError(false);
 
-        const response = await fetch('https://axiobk-001-site1.ktempurl.com/api/Quiz/GetQuizActive');
+        const response = await fetch(
+          "https://axiobk-001-site1.ktempurl.com/api/Quiz/GetQuizActive"
+        );
         if (!response.ok) {
-          throw new Error('Error en la respuesta de la API');
+          throw new Error("Error en la respuesta de la API");
         }
 
         const data = await response.json();
         if (!data || data.length === 0) {
-          throw new Error('No se encontraron datos');
+          throw new Error("No se encontraron datos");
         }
 
         setQuizData(data);
@@ -72,14 +76,22 @@ function Quiz() {
     fetchQuizData();
   }, []);
 
+  // Verificar si está cargando
   if (isFetching) {
-    return <Wait/>; 
+    return <Wait />;
   }
 
+  // Verificar si hay un error o datos vacíos
   if (isError || quizData.length === 0) {
-    return <Maintenance />; // Renderiza la pantalla de mantenimiento si hay error o datos vacíos
+    return <Maintenance />;
   }
 
+  // Verificar si faltan organización o departamento
+  if (!organizationId || !departamentId) {
+    return (
+     <Maintenance />
+    );
+  }
 
   const isNextDisabled = () => {
     if (currentStep === 0) return !organizationId;
@@ -96,8 +108,14 @@ function Quiz() {
     <div>
       <Helmet>
         <title>Servicios - Fundación Axio</title>
-        <meta name="description" content="Descubre los servicios de la Fundación Axio, incluyendo diagnóstico de cultura de integridad, formación y más." />
-        <meta name="keywords" content="Servicios, Fundación Axio, ética, formación, cultura de integridad, desarrollo organizacional" />
+        <meta
+          name="description"
+          content="Descubre los servicios de la Fundación Axio, incluyendo diagnóstico de cultura de integridad, formación y más."
+        />
+        <meta
+          name="keywords"
+          content="Servicios, Fundación Axio, ética, formación, cultura de integridad, desarrollo organizacional"
+        />
         <meta name="author" content="Fundación Axio" />
         <link rel="canonical" href="https://tu-sitio-web.com/services" />
       </Helmet>
@@ -107,8 +125,11 @@ function Quiz() {
       </div>
       <div className="instrucciones-container">
         <p>
-          El presente cuestionario tiene como objetivo develar la cultura organizacional asociada a la ética y los valores en las organizaciones.
-          Tus respuestas son totalmente anónimas y solo solicitamos datos generales con fines estadísticos. El instrumento <strong>NO</strong> pretende evaluar a nadie en lo personal, por lo que te rogamos respondas con total honestidad.
+          El presente cuestionario tiene como objetivo develar la cultura organizacional asociada a
+          la ética y los valores en las organizaciones. Tus respuestas son totalmente anónimas y
+          solo solicitamos datos generales con fines estadísticos. El instrumento <strong>NO</strong>{" "}
+          pretende evaluar a nadie en lo personal, por lo que te rogamos respondas con total
+          honestidad.
         </p>
       </div>
       <div className="App">
@@ -123,14 +144,19 @@ function Quiz() {
               onChange={(e) => {
                 setOrganizationId(e.target.value);
                 const selectedDepartments = quizData
-                  .filter(item => item.OrganizationID === e.target.value)
-                  .map(item => ({ id: item.DepartamentID, name: item.DepartamentName }));
+                  .filter((item) => item.OrganizationID === e.target.value)
+                  .map((item) => ({
+                    id: item.DepartamentID,
+                    name: item.DepartamentName,
+                  }));
                 setDepartments(selectedDepartments);
                 if (e.target.value) handleNext();
               }}
               className="organization-select"
             >
-              <option value="" disabled>Selecciona una organización</option>
+              <option value="" disabled>
+                Selecciona una organización
+              </option>
               {quizData.map((item) => (
                 <option key={item.OrganizationID} value={item.OrganizationID}>
                   {item.OrganizationName}
@@ -146,15 +172,14 @@ function Quiz() {
             <select
               value={departamentId}
               onChange={(e) => {
-                const departmentId = e.target.value;
-                setDepartmentId(departmentId);
-                setSelectedDepartment(departmentId); // Guarda temporalmente el departamento
-                console.log('Departamento seleccionado (selectedDepartment):', departmentId);
-                if (departmentId) handleNext();
+                setDepartmentId(e.target.value);
+                if (e.target.value) handleNext();
               }}
               className="organization-select"
             >
-              <option value="" disabled>Selecciona un departamento</option>
+              <option value="" disabled>
+                Selecciona un departamento
+              </option>
               {departments.map((dept) => (
                 <option key={dept.id} value={dept.id}>
                   {dept.name}
@@ -164,71 +189,10 @@ function Quiz() {
           </div>
         )}
 
-        {currentStep === 2 && organizationId && departamentId && (
-          <div className="question-container">
-            <h3>Selecciona tu género para avanzar</h3>
-            <select
-              value={gener}
-              onChange={(e) => {
-                setGener(e.target.value);
-                if (e.target.value) handleNext();
-              }}
-              className="organization-select"
-            >
-              <option value="" disabled>Selecciona tu género</option>
-              <option value="male">Masculino</option>
-              <option value="female">Femenino</option>
-              <option value="other">Otro</option>
-            </select>
-          </div>
-        )}
-
-        {currentStep === 3 && gener && (
-          <div className="question-container">
-            <h3>Selecciona tu edad</h3>
-            <select
-              value={age}
-              onChange={(e) => {
-                setAge(e.target.value);
-                if (e.target.value) handleNext();
-              }}
-              className="organization-select"
-            >
-              <option value="" disabled>Selecciona tu edad</option>
-              <option value="18-25">18 a 25</option>
-              <option value="26-30">26 a 30</option>
-              <option value="31-50">31 a 50</option>
-              <option value="51+">51 o más</option>
-            </select>
-          </div>
-        )}
-
-        {currentStep === 4 && age && (
-          <div className="question-container">
-            <h3>Selecciona tu nivel de estudios</h3>
-            <select
-              value={educationLevel}
-              onChange={(e) => {
-                setEducationLevel(e.target.value);
-                if (e.target.value) handleNext();
-              }}
-              className="organization-select"
-            >
-              <option value="" disabled>Selecciona tu nivel de estudios</option>
-              <option value="primaria">Primaria</option>
-              <option value="secundaria">Secundaria</option>
-              <option value="preparatoria">Preparatoria</option>
-              <option value="superior">Superior</option>
-              <option value="maestria">Maestría</option>
-              <option value="doctorado">Doctorado</option>
-            </select>
-          </div>
-        )}
-
-        {currentStep >= 5 && currentStep < randomQuestions.length + 5 && (
+        {currentStep >= 2 && currentStep < randomQuestions.length + 5 && (
           <MultipleChoiceQuestion
-            question={randomQuestions[currentStep - 5].question}
-            options={randomQuestions[currentStep - 5].options}
+            question={randomQuestions[currentStep - 5]?.question}
+            options={randomQuestions[currentStep - 5]?.options}
             onOptionSelect={handleOptionSelect}
             selectedOption={answers[currentStep - 5]}
             questionIndex={currentStep - 5}
@@ -239,7 +203,6 @@ function Quiz() {
           <button onClick={handlePrevious} disabled={currentStep === 0} className="qzbutton">
             Anterior
           </button>
-
           {currentStep < randomQuestions.length + 4 ? (
             <button
               onClick={handleNext}
@@ -249,22 +212,9 @@ function Quiz() {
               Siguiente
             </button>
           ) : (
-            <button
-              onClick={handleFinishClick}
-              disabled={answers[currentStep - 5] === null}
-              className="qzbutton"
-            >
+            <button onClick={handleFinishClick} className="qzbutton">
               Finalizar
             </button>
-          )}
-
-          {showFinish && (
-            <Finish
-              onClose={() => setShowFinish(false)}
-              organizationId={organizationId}
-              departamentId={selectedDepartment} // Usamos el estado temporal
-              quizData={quizData}
-            />
           )}
         </div>
       </div>
